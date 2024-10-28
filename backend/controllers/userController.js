b7d4dc66d66ece8d01b5224ab7e9c3aad3ad0701
muhammed favas <146924331+muhammedfavasz@users.noreply.github.com>
2024-10-28 10:29:02 +0530

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,7 +2,7 @@ import userModel from "../models/userModel.js";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import validator from "validator";
-
+const bcrypt = require('bcryptjs');
 //login user
 const loginUser = async (req, res) => {
     const { email, password } = req.body;
