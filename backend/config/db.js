2c975b66e2d805488fc8844bbcdd19982fb644c1
muhammed favas <146924331+muhammedfavasz@users.noreply.github.com>
2024-10-28 12:19:31 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,14 @@
 import mongoose from "mongoose";
 
-export const connectDB= async () => {
-    await mongoose.connect('mongodb+srv://muhammedfavas1609:muhammedfavas1609@cluster0.g9ltb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(()=>{console.log("DB connected");})
-}
+export const connectDB = async () => {
+    try {
+        await mongoose.connect('mongodb+srv://muhammedfavas1609:muhammedfavas1609@cluster0.g9ltb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log("MongoDB connected...");
+    } catch (err) {
+        console.error("Error connecting to MongoDB:", err);
+        process.exit(1); // Exit process if connection fails
+    }
+};
